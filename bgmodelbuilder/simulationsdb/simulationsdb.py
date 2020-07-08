bdc2bb850f29f9c4079c0c275a116ce09a6aed01
Ben Loer <ben.loer@pnnl.gov>
2020-07-08 13:20:17 -0700

    enable json simdb entry uploads

--- bgmodelbuilder/simulationsdb/simulationsdb.py
+++ bgmodelbuilder/simulationsdb/simulationsdb.py
@@ -1,14 +1,14 @@
 """ simulationsdb.py
 
 Defines the base class for a SimulationsDB. Methods must be overloaded by
-the users' concrete derived class. 
+the users' concrete derived class.
 
 A 'conversion efficiency' is the probability that a radioactive decay of a given
-spectrum at a given location in the experiment geometry will produce an event 
-of interest. Most frequently these are determined by simulations. The 
+spectrum at a given location in the experiment geometry will produce an event
+of interest. Most frequently these are determined by simulations. The
 SimulationsDB tracks all known conversion effs and calculates reduced event
-quantities. 
-""" 
+quantities.
+"""
 
 #pythom 2+3 compatibility
 from __future__ import (absolute_import, division,
@@ -25,24 +25,24 @@ from .simdatamatch import SimDataMatch, AssemblyPath
 log = logging.getLogger(__name__)
 
 class SimulationsDB(object):
-    """Abstract class to be implemented by user. 
+    """Abstract class to be implemented by user.
 
-    SimulationsDB serves 2 main purposes: 
-    1. Translate requests for simulation data for a given assembly tree and 
+    SimulationsDB serves 2 main purposes:
+    1. Translate requests for simulation data for a given assembly tree and
        material spec into real database queries and find datasets matching
-       those queries. 
+       those queries.
     2. Calculate quantities (e.g. event rates) for matching datasets
 
     Auxiliary functions include:
-    * listing the types of calculated quantities that the class knows how to 
+    * listing the types of calculated quantities that the class knows how to
       handle (Not yet implemented)
 
     TODO: Add an argument to attach a caching layer here
-    
+
     """
-    
+
     def __init__(self, app=None, *args, **kwargs):
-        """Initialize a new DB instance. 
+        """Initialize a new DB instance.
         Args:
             app: a Flask application object to register with
         """
@@ -65,7 +65,7 @@ class SimulationsDB(object):
         if path is None:
             path = AssemblyPath()
         mymatches = []
-        
+
         for spec in component.getspecs(deep=True):
             if spec.emissionrate(component):
                 res = self.genqueries(SimDataMatch(path,spec), findnewdata)
@@ -77,7 +77,7 @@ class SimulationsDB(object):
 
         if hasattr(component,'components'):
             for child in component.components:
-                mymatches.extend(self._getsimdatamatches(child.component, 
+                mymatches.extend(self._getsimdatamatches(child.component,
                                                          findnewdata,
                                                          path+child))
         return mymatches
@@ -86,9 +86,9 @@ class SimulationsDB(object):
         """Compare a new SimDataMatch with a previously stored one, and update
         the dataset and livetime if necessary
         Args:
-            newmatch (SimDataMatch): The more recently generated object 
+            newmatch (SimDataMatch): The more recently generated object
             oldmatch (SimDataMatch): the previously stored object
-            copydataset (bool): If True, `oldmatch.dataset` will be copied 
+            copydataset (bool): If True, `oldmatch.dataset` will be copied
                                 to `newmatch`, and livetime will be updated
                                 (scaling by the ratio of `emissionrate`
         Returns:
@@ -99,7 +99,7 @@ class SimulationsDB(object):
             newmatch.dataset = oldmatch.dataset
             # update the livetime if need be
             if newmatch.emissionrate != oldmatch.emissionrate:
-                if (newmatch.dataset and 
+                if (newmatch.dataset and
                     (not newmatch.emissionrate or not oldmatch.emissionrate)):
                     log.error(("Null emissionrate for simdatamatch %s, "
                                "unable to calculate livetime"),newmatch.id)
@@ -120,25 +120,33 @@ class SimulationsDB(object):
 
         if newmatch.livetime != oldmatch.livetime:
             newmatch.addstatus("livetimechanged")
-            if (newmatch.livetime > oldmatch.livetime or 
+            if (newmatch.livetime > oldmatch.livetime or
                 oldmatch.livetime is None):
                 newmatch.addstatus("livetimeadded")
-            elif (newmatch.livetime < oldmatch.livetime or 
+            elif (newmatch.livetime < oldmatch.livetime or
                   newmatch.livetime is None):
                 newmatch.addstatus("livetimeremoved")
-        
+
         return newmatch
 
-    def updatesimdata(self, model, findnewmatches=True, findnewdata=True, 
+    def addentry(self, entry, fmt=''):
+        """ Add a new entry to the database. `fmt` should be a string
+        specifying the format of the `entry` object.
+
+        If implemented, suggest to return the primary key id of the new entry
+        """
+        raise NotImplementedError
+
+    def updatesimdata(self, model, findnewmatches=True, findnewdata=True,
                       attach=False):
         """ Generate database queries to find simulation data for this
         model.  If a previous entry for a given query already exists, just
-        update it. 
+        update it.
 
         Args:
             model (BgModel): the model to update
-            findnewmatches (bool): If True, generate queries for all possible 
-                                   placement, spec combinations.  If False, 
+            findnewmatches (bool): If True, generate queries for all possible
+                                   placement, spec combinations.  If False,
                                    only update matches that already exist
             findnewdata (bool): If False, just generate the query but don't
                                 check the database for new hits
@@ -146,7 +154,7 @@ class SimulationsDB(object):
         """
         #First, generate SimDataMatch objects for each path, spec
         newmatches = self._getsimdatamatches(model.assemblyroot, findnewdata)
-                
+
         # For each match, see if one already exists in the model
         # if so, clone some of the prior data, or mark differences
         for newmatch in newmatches:
@@ -158,7 +166,7 @@ class SimulationsDB(object):
                     # we've found a match
                     found=True
                     self._updatestats(newmatch, oldmatch, not findnewdata)
-                    # there should only be one match 
+                    # there should only be one match
                     break
             if not found:
                 newmatch.addstatus('newmatch')
@@ -169,37 +177,37 @@ class SimulationsDB(object):
             model.simdata = {m.id : m for m in newmatches}
         return newmatches
 
-        
+
     def genqueries(self, request, findnewdata=True):
-        """Create SimDataMatch objects with appropriate database queries 
-        for the given path (list of Placements) and emission spec. 
+        """Create SimDataMatch objects with appropriate database queries
+        for the given path (list of Placements) and emission spec.
         To be implemented by the concrete database class
-        
+
         Args:
             request (SimDataMatch): An empty simdatamatch object, containing
                                     AssemblyPath and Emission spec to generate
                                     queries for
             findnewdata (bool): If True, the generated query should be executed
-                                and the resulting dataset(s) attached to each 
+                                and the resulting dataset(s) attached to each
                                 SimDataMatch object returned, and the livetime
                                 calculated appropriately.
         Returns:
-            matches (list): a list of SimDataMatch objects for this request. 
+            matches (list): a list of SimDataMatch objects for this request.
         If the concrete DB doesn't know how to generate a query for these
-        parameters, either return an empty list, or better yet, return 
-        a list containing `request` with status containing "error" and log an 
-        appropriate error message to help with debugging.  
-        In some cases requests will generate more than one match, e.g. U238 
-        and Th232 specs may generate both gamma and neutron emission matches. 
+        parameters, either return an empty list, or better yet, return
+        a list containing `request` with status containing "error" and log an
+        appropriate error message to help with debugging.
+        In some cases requests will generate more than one match, e.g. U238
+        and Th232 specs may generate both gamma and neutron emission matches.
         Additional matches should be generated by calling `request.clone()`
-        In this case, be sure to set the resulting weight appropriately. 
+        In this case, be sure to set the resulting weight appropriately.
         """
         return NotImplementedError
 
-        
+
     def evaluate(self, values, matches):
         """Evaluate the reduced sum for values over the list of emissionspecs.
-        To be implemented by the concrete database instance. 
+        To be implemented by the concrete database instance.
 
         Args:
             values (list): list of identifiers for values. E.g., names
@@ -208,19 +216,19 @@ class SimulationsDB(object):
                             to caluclate these values for
 
         Returns:
-            result (list): list of computed results in same order as values. 
+            result (list): list of computed results in same order as values.
 
         TODO: How to distinguish incorrect vs empty value requests?
         """
         raise NotImplementedError
-                    
-        
+
+
     def getdatasetdetails(self, datasetid):
         """Return an object with detailed info about a dataset
-        To be implemented by the concrete database instance. 
+        To be implemented by the concrete database instance.
         """
         raise NotImplementedError
-    
+
     def defaultquery(self, request):
         """Generate the default query for the associated component, spec"""
         pass
@@ -229,12 +237,12 @@ class SimulationsDB(object):
         """Run the query against the DB. Return a list of datasets or ids
         Args:
             query: Query to apply to the underlying database
-            projection: a projection/mapping operator 
+            projection: a projection/mapping operator
             sort: a sorting operator
         """
         pass
-        
-    
+
+
     def init_app(self, app):
         """Register ourselves as the official SimulationsDB extension"""
         app.extensions['SimulationsDB'] = self
