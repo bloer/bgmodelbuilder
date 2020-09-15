c19de4a49369f37bd4473f799c12ad43e4306152
Ben Loer <ben.loer@pnnl.gov>
2020-09-15 13:24:32 -0700

    prepare 0.4 release

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='bgmodelbuilder',
-      version='0.3.2',
+      version='0.4',
       description='Tools for building radioactive background models',
       url='http://github.com/bloer/bgmodelbuilder',
       author='Ben Loer',
