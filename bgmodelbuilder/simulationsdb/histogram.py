049c4ea9677daf0f354c413b389881eaab10e2cf
Ben Loer <ben.loer@pnnl.gov>
2021-07-13 14:43:54 -0700

    fix typo

--- bgmodelbuilder/simulationsdb/histogram.py
+++ bgmodelbuilder/simulationsdb/histogram.py
@@ -4,34 +4,34 @@ from ..common import units
 
 class Histogram(object):
     """2-tuple mimicking np.histogram structure, with operator overloads
-    so that bins are not added/scaled etc 
+    so that bins are not added/scaled etc
     """
     def __init__(self, hist, bin_edges=None):
-        # todo: make sure we don't steal an array? 
+        # todo: make sure we don't steal an array?
         if isinstance(hist, units.Quantity):
             if not isinstance(hist.m, np.ndarray):
                 hist = units.Quantity(np.array(hist.m), hist.u)
         elif not isinstance(hist, np.ndarray):
             hist = np.array(hist)
-            
+
         self.hist = hist
         self.bin_edges = bin_edges
         if bin_edges is None:
             self.bin_edges = np.arange(len(self.hist))
-            
+
 
     def find_bin(self, x):
         """Find the index of the bin where x is.
-        Args: 
+        Args:
             x (float): value to search for. must have same units as bins
         Returns:
             bin (int): will be -1 if before first bin, Nbins if outside last bin
         """
         return self.bin_edges.searchsorted(x, 'right')-1
-        
+
     def val(self, x, interp=None):
-        """Get the value of the histogram at x. x must be in the same 
-        units as the bins. 
+        """Get the value of the histogram at x. x must be in the same
+        units as the bins.
         Args:
             x (float): Value to test
             interp (str): Currently: if truthy, linearly interpolate the value
@@ -59,25 +59,25 @@ class Histogram(object):
         if b is None or b >= bins[-1]:
             b = bins[-1]
         return (a, b)
-        
+
     def integrate(self, a=None, b=None, binwidth=True):
-        """Integrate the histogram from a to b. If a and b do not correspond to 
-        exact bin edges, the value in the bin will be interpolated. 
+        """Integrate the histogram from a to b. If a and b do not correspond to
+        exact bin edges, the value in the bin will be interpolated.
 
         Args:
-            a (float): low range. If bin_edges has units, this must have the 
+            a (float): low range. If bin_edges has units, this must have the
                        same dimensionality. IF bin_edges is None, this will
                        be treated as a bin index. if None, use low bound
             b (float): upper edge. if None, use upper bound
             binwidth (bool): if True (default), multiply each bin by the bin
                              width. If False, just add bin values
         """
-        bins = self.bin_edges 
+        bins = self.bin_edges
         spec = self.hist
         a, b = self._bound(a,b)
         if binwidth:
-            spec = spec * (bins[1:]-bins[:-1]) 
-        
+            spec = spec * (bins[1:]-bins[:-1])
+
         first = bins.searchsorted(a,"right")
         last = bins.searchsorted(b,"left")
         #take fractions of the first and last bins
@@ -95,7 +95,7 @@ class Histogram(object):
         a, b = self._bound(a,b)
         return self.integrate(a, b, binwidths) / (b-a)
 
-    
+
     def _testbins(self, other):
         try:
             otherbins = other.bin_edges
@@ -112,9 +112,9 @@ class Histogram(object):
         return self.bin_edges
 
     def _combine(self, other, op, inplace=False):
-        #treat None as zero 
-        
-        #make sure bins are equal 
+        #treat None as zero
+
+        #make sure bins are equal
         bins = self._testbins(other)
 
         try:
@@ -128,9 +128,9 @@ class Histogram(object):
         else:
             return self.__class__(op(self.hist,otherhist), bins)
 
-        
+
     #todo: should we provide for adding raw spectra rather than just Histograms?
-    #binary copy operators    
+    #binary copy operators
     def __add__(self, other):
         if other is None:
             other = 0
@@ -138,56 +138,56 @@ class Histogram(object):
             return self._combine(other, np.add)
         except units.errors.DimensionalityError:
             return self._combine(other, operator.add)
-     
+
     def __sub__(self, other):
         if other is None:
             other = 0
         return self._combine(other, np.subtract)
-    
+
     def __mul__(self, other):
         if other is None:
             other = 0
         return self._combine(other, np.multiply)
-    
+
     def __floordiv__(self, other):
         return self._combine(other, np.floor_divide)
-    
+
     def __truediv__(self, other):
         return self._combine(other, np.true_divide)
-    
+
     def __mod__(self, other):
         return self._combine(other, np.mod)
-    
+
     def __pow__(self, other):
         return self._combine(other, np.power)
-    
+
     #do we need logical/bitwise operators??
-        
+
     #binary in-place operators
     def __iadd__(self, other):
         if other is None:
             other = 0
         return self._combine(other, np.add, inplace=True)
-     
+
     def __isub__(self, other):
         if other is None:
             other = 0
         return self._combine(other, np.subtract, inplace=True)
-    
+
     def __imul__(self, other):
         if other is None:
             other = 0
         return self._combine(other, np.multiply, inplace=True)
-    
+
     def __ifloordiv__(self, other):
         return self._combine(other, np.floor_divide, inplace=True)
-    
+
     def __itruediv__(self, other):
-        return self._combine(other, np.true_divice, inplace=True)
-    
+        return self._combine(other, np.true_divide, inplace=True)
+
     def __imod__(self, other):
         return self._combine(other, np.mod, inplace=True)
-    
+
     def __ipow__(self, other):
         return self._combine(other, np.power, inplace=True)
 
@@ -197,38 +197,38 @@ class Histogram(object):
         if other is None:
             other = 0
         return self.__class__(other + self.hist, self.bin_edges)
-     
+
     def __rsub__(self, other):
         if other is None:
             other = 0
         return self.__class__(other - self.hist, self.bin_edges)
-    
+
     def __rmul__(self, other):
         if other is None:
             other = 0
         return self.__class__(other * self.hist, self.bin_edges)
-    
+
     def __rfloordiv__(self, other):
         return self.__class__(other // self.hist, self.bin_edges)
-    
+
     def __rtruediv__(self, other):
         return self.__class__(other / self.hist, self.bin_edges)
-    
+
     def __rmod__(self, other):
         return self.__class__(other % self.hist, self.bin_edges)
-    
+
     def __rpow__(self, other):
         return self.__class__(other ** self.hist, self.bin_edges)
-    
-    
-    
+
+
+
     #unary operators
     def __neg__(self):
         return self.__class__(-self.hist, self.bin_edges)
-    
+
     def __abs__(self):
         return self.__class__(np.abs(self.hist), self.bin_edges)
-        
-    
-    
-    
+
+
+
+
